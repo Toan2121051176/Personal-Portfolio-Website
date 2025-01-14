60de26a72d7f3607bf9da80ab13d0005f36841e2
Toan2121051176 <toansd9a@gmail.com>
2025-01-14 17:18:59 +0700

    Toàn

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavbarHook from "./NavbarHook/NavbarHook";
+import Home from "./pages/Home";
+import News from "./pages/News";
+
+const App = () => {
+  return (
+    <Router>
+      {/* <Navbar /> */}
+      <NavbarHook />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/news" element={<News />} />
+          {/* Define other routes that you need*/}
+        </Routes>
+      </main>
+    </Router>
+  );
+};
+
+export default App;

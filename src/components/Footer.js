4e363435602b4064586d66debdf2b82e4a4f0dea
Toan2121051176 <toansd9a@gmail.com>
2025-01-16 09:47:33 +0700

    sửa

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,9 @@ const Footer = () => {
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
         <div className="flex flex-col md:flex-row md:space-x-12 items-center mb-4">
           <div className="flex-1 mb-4 md:mb-0">
-            <h3 className="text-2xl font-bold mb-2">Yousaf</h3>
+            <h3 className="text-2xl font-bold mb-2">Toàn Báo</h3>
             <p className="text-gray-400">
-              Full-Stack Developer based in the USA, specializing in web and
-              software development.
+            Lập trình viên Full-Stack có trụ sở tại , chuyên về phát triển web và phần mềm.
             </p>
           </div>
           <div className="flex-1 w-full">

daccc29bfb365e0266ae4fe7fa5c1dc513f3a3ef
Toan2121051176 <toansd9a@gmail.com>
2025-01-16 11:26:00 +0700

    thêm map

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -78,6 +78,8 @@ const Footer = () => {
           </div>
         </div>
       </div>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.0662282781477!2d105.7725698!3d21.0700169!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3134552fa8723d05%3A0x5ee2b56189f2dc9c!2zMzkgUC4gVmnDqm4sIEPhu5UgTmh14bq_LCBC4bqvYyBU4burIExpw6ptLCBIw6AgTuG7mWk!5e0!3m2!1svi!2s!4v1737001155571!5m2!1svi!2s"/>
+
     </footer>
   );
 };
